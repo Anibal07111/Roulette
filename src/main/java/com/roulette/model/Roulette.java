79d400172ac0800e72b681ee910867f6f3d95454
DESKTOP-VPEUULT\MI PC <amunozg_1@uqvirtual.edu.co>
2021-10-18 13:24:09 -0500

    Prueba Masiv

---
+++ src/main/java/com/roulette/model/Roulette.java
@@ -0,0 +1,37 @@
+package com.roulette.model;
+
+import java.util.HashMap;
+
+import org.springframework.data.annotation.Id;
+
+public class Roulette {
+	@Id
+	private Long idRoulette;
+	private HashMap<Integer, Bet> betList;
+	private String status;	
+	public Roulette(String status) {
+		this.status = status;
+	}
+	public Roulette() {
+		betList = new HashMap<>();
+	}
+	public Long getIdRoulette() {
+		return idRoulette;
+	}
+	public void setIdRoulette(Long idRoulette) {
+		this.idRoulette = idRoulette;
+	}
+	public HashMap<Integer, Bet> getBetList() {
+		return betList;
+	}
+	public void setBetList(HashMap<Integer, Bet> betList) {
+		this.betList = betList;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
